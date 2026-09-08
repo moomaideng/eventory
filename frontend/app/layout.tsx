e71fdaa3f98acab73bb30d57c9560a5255cae54f
haminic <hamkittichet@gmail.com>
2026-09-09 00:28:07 +0700

    feat: new product theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Outfit, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { RoleProvider } from "@/context/role-context";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
-const inter = Inter({
+const fontSans = Outfit({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +30,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full font-sans antialiased", inter.variable)}
+      className={cn(
+        "h-full font-sans antialiased",
+        fontSans.variable,
+        fontMono.variable
+      )}
       suppressHydrationWarning
     >
       <body className="bg-background text-foreground flex min-h-full flex-col">
